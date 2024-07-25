4ba112d8c570ec7230041689505f2957e8f9a7d8
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-07-24 16:31:36 -1000

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Arseniy Oksenoyt on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
